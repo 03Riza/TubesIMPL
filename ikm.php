18245a41693facd1f13dc24d1d8e0a744ab36a7d
adityamohamad <48940215+adityamohamad@users.noreply.github.com>
2022-12-14 17:40:08 +0700

    mengupdate ikm.php

--- ikm.php
+++ ikm.php
@@ -18,7 +18,7 @@
   </head>
 <body>
 <?php
-require_once('koneksi.php');
+require_once('koneksiIKM.php');
 
 ?>
  <div id="wrapper">
@@ -30,21 +30,35 @@ include"header.php";
 <div id="page-wrapper">
 <div class="row" id="contentInput" >
   <div class="col-lg-12">
-    <h1>Instalasi Kepuasan Masyarakat</h1>
+    <h1>Index Kepuasan Masyarakat</h1>
       <ol class="breadcrumb">
         <li class="active"></li>
       </ol>
   </div>
 </div>
 
+  <style type="text/css">
+    .box {
+      padding: 30px 40px;
+      border-radius: 10px;
+    }
+  </style>
+
+<style type="text/css">
+    .box {
+      padding: 30px 40px;
+      border-radius: 10px;
+    }
+  </style>
+
 <?php
   include 'koneksiIKM.php';
   
-  $tampil = mysqli_query($koneksi, "SELECT * FROM `ikm`");
+  $tampil = mysqli_query($koneksi, "SELECT * FROM db_ikm");
   $data = mysqli_fetch_array($tampil);
 ?>
 
-<!----- Container ----->
+  <!----- Container ----->
   <div class="container">
     <div class="alert alert-warning" role="alert"> 
       Pilihlah salah satu kepuasan kalian dalam pelayanan yang telah diberikan! 
@@ -60,7 +74,9 @@ include"header.php";
               <h5>Orang</h5>
             </div>
             <div class="col-md-4">
-              
+              <a href="simpan.php?ket=memuaskan" title="Jika anda merasa nyaman/puas dengan pelayanan yang sudah diberikan maka klik icon ini">
+                <img src="img/memuaskan.png" style="width: 75px;">
+              </a>
             </div>
           </div>
         </div>
@@ -74,6 +90,11 @@ include"header.php";
               <h2>[<?=$data['cukup']?>]</h2>
               <h5>Orang</h5>
             </div>
+             <div class="col-md-4">
+              <a href="simpan.php?ket=cukup" title="Jika anda merasa nyaman/cukup dengan pelayanan yang sudah diberikan maka klik icon ini">
+                <img src="img/cukup.png" style="width: 75px;">
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -86,6 +107,10 @@ include"header.php";
               <h2>[<?=$data['kurang']?>]</h2>
               <h5>Orang</h5>
             </div>
+             <div class="col-md-4">
+              <a href="simpan.php?ket=kurang" title="Jika anda merasa kurang nyamandengan pelayanan yang sudah diberikan maka klik icon ini">
+                <img src="img/kurang.png" style="width: 75px;">
+              </a>
           </div>
         </div>
       </div>
